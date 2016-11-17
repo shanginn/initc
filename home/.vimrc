378e36cf6ad69f7aceed53f0c612fd60471b396b
Николай Шангин <shanginn@gmail.com>
2016-11-17 16:24:15 +0500

    some

---
+++ home/.vimrc
@@ -0,0 +1,89 @@
+" Necesary  for lots of cool vim things
+set nocompatible
+filetype off                  " required
+
+" This shows what you are typing as a command.  I love this!
+set showcmd
+
+" Folding Stuffs
+set foldmethod=marker
+
+" Needed for Syntax Highlighting and stuff
+filetype on
+filetype plugin on
+syntax enable
+set grepprg=grep\ -nH\ $*
+
+" Who doesn't like autoindent?
+" set autoindent
+
+" Spaces are better than a tab character
+set expandtab
+set smarttab
+
+" Who wants an 8 character tab?  Not me!
+set shiftwidth=2
+set softtabstop=2
+
+" Got backspace?
+set backspace=2
+
+" Line Numbers PWN!
+set number
+
+" Ignoring case is a fun trick
+set ignorecase
+
+" Incremental searching is sexy
+set incsearch
+
+" Highlight things that we find with the search
+set hlsearch
+
+" When I close a tab, remove the buffer
+set nohidden
+
+set laststatus=2
+set statusline=%F%m%r%h%w\ (%{&ff}){%Y}\ [%l,%v][%p%%]
+
+" Show matching brackets when text indicator is over them
+set showmatch
+" How many tenths of a second to blink when matching brackets
+set mat=2
+
+" No annoying sound on errors
+set noerrorbells
+set novisualbell
+set t_vb=
+set tm=500
+
+colorscheme mustang
+
+set list
+set listchars=tab:――,trail:·,extends:#,nbsp:·
+
+function! ResCur()
+  if line("'\"") <= line("$")
+    normal! g`"
+    return 1
+  endif
+endfunction
+
+augroup resCur
+  autocmd!
+  autocmd BufWinEnter * call ResCur()
+augroup END
+
+
+" set the runtime path to include Vundle and initialize
+set rtp+=~/.vim/bundle/Vundle.vim
+call vundle#begin()
+
+" let Vundle manage Vundle, required
+Plugin 'VundleVim/Vundle.vim'
+
+Plugin 'wakatime/vim-wakatime'
+
+" All of your Plugins must be added before the following line
+call vundle#end()            " required
+filetype plugin indent on    " required
---
+++ tools/install.sh
@@ -0,0 +1,72 @@
+main() {
+  # Use colors, but only if connected to a terminal, and that terminal
+  # supports them.
+  if which tput >/dev/null 2>&1; then
+      ncolors=$(tput colors)
+  fi
+  if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+    RED="$(tput setaf 1)"
+    GREEN="$(tput setaf 2)"
+    YELLOW="$(tput setaf 3)"
+    BLUE="$(tput setaf 4)"
+    BOLD="$(tput bold)"
+    NORMAL="$(tput sgr0)"
+  else
+    RED=""
+    GREEN=""
+    YELLOW=""
+    BLUE=""
+    BOLD=""
+    NORMAL=""
+  fi
+
+  # Only enable exit-on-error after the non-critical colorization stuff,
+  # which may fail on systems lacking tput or terminfo
+  set -e
+
+  # Prevent the cloned repository from having insecure permissions. Failing to do
+  # so causes compinit() calls to fail with "command not found: compdef" errors
+  # for users with insecure umasks (e.g., "002", allowing group writability). Note
+  # that this will be ignored under Cygwin by default, as Windows ACLs take
+  # precedence over umasks except for filesystems mounted with option "noacl".
+  umask g-w,o-w
+
+  printf "${BLUE}Cloning InitC repository...${NORMAL}\n"
+  hash git >/dev/null 2>&1 || {
+    echo "Error: git is not installed"
+    exit 1
+  }
+  # The Windows (MSYS) Git is not compatible with normal use on cygwin
+  if [ "$OSTYPE" = cygwin ]; then
+    echo "Not for Windows, sorry"
+    exit 1
+  fi
+
+  if [ ! -n "$INITC" ]; then
+    INITC=~/.initc
+  fi
+
+  env git clone --depth=1 https://github.com/shanginn/initc.git $INITC || {
+    printf "Error: git clone of InitC repo failed\n"
+    exit 1
+  }
+
+  $INITC/initc
+
+  printf "${GREEN}"
+  echo '__/\\\\\\\\\\\______________________________________________/\\\\\\\\\_________'
+  echo '__\/////\\\///____________________________________________/\\\////////_________'
+  echo '_______\/\\\______________________/\\\______/\\\_________/\\\/_________________'
+  echo '________\/\\\_______/\\/\\\\\\____\///____/\\\\\\\\\\\___/\\\__________________'
+  echo '_________\/\\\______\/\\\////\\\____/\\\__\////\\\////___\/\\\_________________'
+  echo '__________\/\\\______\/\\\__\//\\\__\/\\\_____\/\\\_______\//\\\_______________'
+  echo '___________\/\\\______\/\\\___\/\\\__\/\\\_____\/\\\_/\\____\///\\\____________'
+  echo '_________/\\\\\\\\\\\__\/\\\___\/\\\__\/\\\_____\//\\\\\_______\////\\\\\\\\\__'
+  echo '_________\///////////___\///____\///___\///_______\/////___________\/////////__'
+  echo ''
+  echo 'Console is ready for use!'
+  printf "${NORMAL}"
+  env zsh
+}
+
+main
