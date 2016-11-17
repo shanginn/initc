eee6120d8782dacbd17f1fc853d7196e7fd84054
Николай Шангин <shanginn@gmail.com>
2016-11-17 16:38:18 +0500

    Force install packages

--- tools/install.sh
+++ tools/install.sh
@@ -71,7 +71,7 @@ install_deps () {
   curl -O https://raw.githubusercontent.com/shanginn/initc/master/tools/pacapt
   chmod +x pacapt
   ./pacapt -Sy
-  ./pacapt -S curl vim htop git zsh
+  ./pacapt -S -y curl vim htop git zsh
   rm -f pacapt
 }
 
